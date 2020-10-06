fc3705669f4ad4fff6349bf4f62a4926b6602e3f
arjunvenkat180003 <56486656+arjunvenkat180003@users.noreply.github.com>
2020-10-06 15:45:59 -0500

    Create chal0.py

---
+++ chal-0/chal0.py
@@ -0,0 +1,41 @@
+import requests
+
+
+url = "https://us-central1-acm-core.cloudfunctions.net/challenge/tags/linux"
+
+
+
+obj = {"name" : "linux", "contents" : "I'd just like to interject for a moment. What you're referring to as Linux is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux..."}
+
+resp = requests.post(url, json=obj)
+print("Post :")
+print(resp.json())
+
+URLtoken = resp.json()["token"]
+
+
+url2 = url+"/"+URLtoken
+
+
+resp2 = requests.get(url2)
+print("Get: ")
+print(resp2.json())
+
+
+
+obj2 = {"name": "linux", "contents": "something else"}
+
+resp3 = requests.patch(url2, data=obj2)
+
+print("Patch: ")
+
+print(resp3.json())
+
+resp4 = requests.delete(url2)
+
+print("Delete: ")
+
+print(resp4.status_code)
+
+if resp4.status_code == 200:
+    print("ok")
